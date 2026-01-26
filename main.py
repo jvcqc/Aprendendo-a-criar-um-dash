379a9be86415b2aaf70bb4614c2a0804edafaddb
JOAO CAVALCANTE <113143562+jvcqc@users.noreply.github.com>
2026-01-26 10:36:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -33,21 +33,21 @@ fig1 = px.histogram(df_filtered, x='ANO', y=['Orcamento_Atualizado', 'Orcamento_
 title='Grupo de Natureza de Despesa Pessoal e Encargos Sociais',
 barmode = 'group', text_auto=True)
 fig1.update_layout(xaxis_title = 'Ano', yaxis_title = 'Valor em R$')
-fig1.show()
+
 
 fig2 = px.histogram(df_filtered, x='ANO', y=['Orcamento_Atualizado', 'Orcamento_Realizado'],
 title='Grupo de Natureza de Despesa Corrente',
 barmode = 'group', text_auto=True,
 color_discrete_map={'Orcamento_Atualizado': 'blue', 'Orcamento_Realizado': 'red'})
 fig2.update_layout(xaxis_title = 'Ano', yaxis_title = 'Valor em R$')
-fig2.show()
+
 
 fig3 = px.histogram(df_filtered, x='ANO', y=['Orcamento_Atualizado', 'Orcamento_Realizado'],
 title='Grupo de Natureza de Despesa Investimento',
 barmode = 'group', text_auto=True,
 color_discrete_map={'Orcamento_Atualizado': 'blue', 'Orcamento_Realizado': 'red'})
 fig3.update_layout(xaxis_title = 'Ano', yaxis_title = 'Valor em R$')
-fig3.show()
+
 
 # Seleciona as colunas E, F e G (índices 4, 5 e 6) e as linhas 31 a 37
 df_filtered = df.iloc[30:37, 4:7]
@@ -73,21 +73,21 @@ title='Comparativo da Despesa de Pessoal e Encargos Sociais - 2022',
 barmode = 'group', text_auto=True,
 color_discrete_map={'Orcamento_Inicial': 'blue', 'Orcamento_Atualizado': 'red'})
 fig4.update_layout(yaxis_title = 'Valor em R$')
-fig4.show()
+
 
 fig5 = px.histogram(df_filtered, x='AÇÃO', y=['Orcamento_Inicial', 'Orcamento_Atualizado'],
 title='Comparativo da Despesa de Pessoal e Encargos Sociais - 2023',
 barmode = 'group', text_auto=True,
 color_discrete_map={'Orcamento_Inicial': 'blue', 'Orcamento_Atualizado': 'red'})
 fig5.update_layout(yaxis_title = 'Valor em R$')
-fig5.show()
+
 
 fig6 = px.histogram(df_filtered, x='AÇÃO', y=['Orcamento_Inicial', 'Orcamento_Atualizado'],
 title='Comparativo da Despesa de Pessoal e Encargos Sociais - 2024',
 barmode = 'group', text_auto=True,
 color_discrete_map={'Orcamento_Inicial': 'blue', 'Orcamento_Atualizado': 'red'})
 fig6.update_layout(yaxis_title = 'Valor em R$')
-fig6.show()
+
 
 df_filtered = df.copy()
 
@@ -109,7 +109,7 @@ title='Comparativo de Despesa Corrente',
 barmode = 'group', text_auto=True,
 color_discrete_map={'SUM de Orçamento Atualizado': 'blue', 'SUM de Orçamento Realizado': 'red'})
 fig7.update_layout(yaxis_title = 'Valor em R$')
-fig7.show()
+
 
 st.plotly_chart(fig1, use_container_width=True)
 st.plotly_chart(fig2, use_container_width=True)
