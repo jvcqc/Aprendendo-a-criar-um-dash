5da3e53efa7036c7c331f481b89f15c012c1f830
JOAO CAVALCANTE <113143562+jvcqc@users.noreply.github.com>
2026-01-27 07:33:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -124,4 +124,33 @@ with row2_col1:
                         
     color_discrete_map={'Orcamento_Atualizado': 'blue', 'Orcamento_Realizado': 'red'})
     fig3.update_layout(xaxis_title = 'Ano', yaxis_title = 'Valor em R$')
+    
     st.plotly_chart(fig3, use_container_width=True)
+
+with row2_col1:
+    # Seleciona as colunas E, F e G (índices 4, 5 e 6) e as linhas 31 a 37
+    df_filtered = df.iloc[30:37, 4:7]
+    
+    # Renomeia as colunas para facilitar o uso
+    df_filtered.columns = ['AÇÃO', 'Orcamento_Inicial', 'Orcamento_Atualizado']
+    
+    # Função para limpar e converter colunas de moeda
+    def clean_currency(column):
+        return (column.astype(str)
+                .str.replace('R$', '', regex=False)
+                .str.replace('.', '', regex=False)
+                .str.replace(',', '.', regex=False)
+                .str.strip()
+                .astype(float))
+    
+    # Aplica a limpeza às colunas de orçamento
+    df_filtered['Orcamento_Atualizado'] = clean_currency(df_filtered['Orcamento_Atualizado'])
+    df_filtered['Orcamento_Inicial'] = clean_currency(df_filtered['Orcamento_Inicial'])
+    
+    fig4 = px.histogram(df_filtered, x='AÇÃO', y=['Orcamento_Inicial', 'Orcamento_Atualizado'],
+    title='Comparativo da Despesa de Pessoal e Encargos Sociais - 2022',
+    barmode = 'group', text_auto=True,
+    color_discrete_map={'Orcamento_Inicial': 'blue', 'Orcamento_Atualizado': 'red'})
+    fig4.update_layout(yaxis_title = 'Valor em R$')
+
+    st.plotly_chart(fig4, use_container_width=True)
