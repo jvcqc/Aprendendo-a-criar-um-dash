f162c76196570cb10b6b2a452e07b2f37732a637
JOAO CAVALCANTE <113143562+jvcqc@users.noreply.github.com>
2026-01-27 07:29:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -84,6 +84,8 @@ with col2:
     
     st.plotly_chart(fig2, use_container_width=True)
 
+st.divider()
+
 row2_col1, row2_col2 = st.columns(2)
 with row2_col1:
     # Seleciona as colunas L, M e N (índices 11, 12 e 13)
